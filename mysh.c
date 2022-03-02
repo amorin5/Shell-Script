638896cb344239c4bb41c90e67c7a893617171ac
Angie Morin <angie@royal-08.cs.wisc.edu>
2022-03-01 18:25:00 -0600

    alias functionality

--- mysh.c
+++ mysh.c
@@ -6,12 +6,29 @@
 #include<sys/wait.h>
 //#include "linkedList.c"
 
+
+struct node {
+   char *data;
+   char *key;
+   struct node *next;
+};
+
+
+void push(struct node* next, char *new_data, char *new_key){
+    printf("called\n");
+    struct node *new_node = malloc(sizeof(struct node));
+    new_node->data  = new_data;
+    new_node->key = new_key;
+    new_node->next = next;
+    next = new_node;
+}
+
 //TO-DOs
 //1. string concatenation to write function
 //2. linked list for alias method (struct)
 //3. redirection method
 //4. unalias
-//5. write the errors
+//6. the 
 
 void printPrompt(){
     write(STDOUT_FILENO, "mysh> ", strlen("mysh> "));                
@@ -21,14 +38,16 @@ void printPrompt(){
 int newProcess(char *myargs[]){
     //write(1, myargs[0], strlen(myargs[0]));
     //write(1, myargs[1], strlen(myargs[1]));
+    printf("NEW PROCESS RUN\n");
     int rc = fork();
+    int status;
     if (rc < 0) {   //Fork Error
         exit(1);
     } else if(rc == 0){
         execv(myargs[0], myargs);
     }
     else {
-       rc = wait(NULL);
+       rc = waitpid(rc, &status, 0);
     }
     return 0;
 }
@@ -56,6 +75,11 @@ int main(int argc, char *argv[]){
     char buffer[512];
     char *myargs[sizeof(buffer)];
     char *token;
+    struct node *head = NULL;
+    //struct node *head = malloc(sizeof(struct node));
+    //struct node *next = malloc(sizeof(struct node));
+    
+    int size = 1;
     while(fgets(buffer, 512, fp)){
         if(flag == 1){
             write(1, buffer, strlen(buffer));
@@ -71,36 +95,58 @@ int main(int argc, char *argv[]){
                 i++;
             }
             //redirection method -- better to do here or easier to do in new call?
-        } else if(strstr(buffer, "alias") != NULL){
-            //struct node *linkedList = (struct node*) malloc(sizeof(struct node));
-
-            //deliminate based on alias -- only 3 args so only the first 2 whitespaces
-            //alias method
-            //check if second argument is alias, unalias, or exit -- error
-            //check if arg[0] is unalias or alias, then determine if add/subtract from LL
         } else {
-            // MODIFIES MYARGS
-            //check if arg[0] is in the linked list of alias
-
             //base case -- break the line into array of arguments
             token = strtok(buffer, " \n");
             myargs[0] = token;
-            int i = 1;
             while(token != NULL){
                 token = strtok(NULL, " \n");
-                myargs[i] = token;
-                i++;
+                myargs[size] = token;
+                size++;
             }
-            
+                //check if arg[0] belongs to the linked list
+            if(head != NULL){
+                struct node *current = malloc(sizeof(struct node));
+                current = head;
+            //printf("%d\n", size);
+                printf("%s Hello\n", head->key);
+                while(current != NULL){
+                    printf("Hello\n");
+                    if(strstr(myargs[0], current->key) != NULL){
+                        myargs[0] = current->data;
+                        myargs[1] = 0;
+                        printf("MYARGS0: %s MYARGS1: %s\n", myargs[0], myargs[1]);
+                        break;
+                    }
+                current = current->next;
+                }
+            }   
+            if(strstr(myargs[0], "alias") != NULL){
+                printf("entered\n");
+                if(head == NULL){
+                    head = malloc(sizeof(struct node));
+                    head->key = myargs[1];
+                    head->data = myargs[2];
+                    head->next = NULL;
+                }
+                // } else if(next->key == NULL) {
+                //     next->key = myargs[1];
+                //     next->data = myargs[2];
+                // } else {
+                //     push(next, myargs[1], myargs[2]);
+                // }
+                continue;
+            }
         }
         newProcess(myargs);
         if(argc == 1){
-            printPrompt();
+            printPrompt(); 
             continue;
         }
+        
     }
     return(0);
-    }
+}
 
     //Batch mode - arg[1] is the name of a file
     //1. check if file is valid, output error if not
