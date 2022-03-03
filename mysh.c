c317416fe42c5054c079845873c2575114c68756
Dana Schneck <schneck@royal-12.cs.wisc.edu>
2022-03-02 20:04:47 -0600

    reformatted and ready to start alias

--- mysh.c
+++ mysh.c
@@ -21,7 +21,7 @@
 struct alias {
     struct alias *nextAlias;
     char *aliasName;
-    char *argv[512];
+    char *argv[256];
     
 };
 
@@ -49,162 +49,130 @@ int checkWhiteSpace(char *buffer) {
     return whitespace;
 }
 
-int newProcess(char *myargs[]) {
-    // printf("NEW PROCESS RUN\n");
+void newProcess(char *myargs[]) {
     int rc = fork();
     int status;
     if (rc < 0) { // Fork Error
         exit(1);
     }
     else if (rc == 0) {
-        execv(myargs[0], myargs);
+        execv(myargs[0], myargs);;
+        _exit(0);
     }
     else {
-        rc = waitpid(rc, &status, 0);
+        waitpid(rc, &status, 0);
     }
-    return 0;
 }
 
-int alias(char *myargs[], int argc) {
+void alias(char *myargs[], int argc) {
     if (argc == 1) {
-        printf("It's just allias\n");
+        printf("It's just alias\n");
     }
     else if (argc == 2) {
-        printf("Check if alias exists and print it out of it does");
+        printf("Check if alias exists and print it out of it does\n");
     }
     else {
-        printf("Deal with adding the alias");
+        printf("Deal with adding the alias\n");
     }
-    return 0;
 }
 
-int unalias(char* myargs[], int argc) {
+void unalias(char* myargs[], int argc) {
     char unaliasError[] = "unalias: Incorrect number of arguments.\n";
 
     if (argc <= 1 || argc > 2) {
         write(1, unaliasError, strlen(unaliasError));
-        return 1;
     }
     else {
-        printf("check if alias exists and delete if it does");
+        printf("check if alias exists and delete if it does\n");
     }
-    return 0;
+}
+
+void processCommand(char* buffer) {
+    char *myargs[256];
+    char *token;
+    const char delim[] = " \n\t\v\t\r";
+
+    token = strtok(buffer, delim);
+    int count = 0;
+    
+    while (token != NULL) {
+        myargs[count] = token;
+        count++;
+        token = strtok(NULL, delim);
+    }
+    myargs[count] = NULL;
+
+    if (strcmp(myargs[0], "alias") == 0) {
+        //printf("alias mode\n");
+        alias(myargs, count);
+    }
+    else if (strcmp(myargs[0], "unalias") == 0) {
+        //printf("unalias mode\n");
+        unalias(myargs, count);
+    }
+    else {
+        // check if arg[0] belongs to the linked list
+        // if (head != NULL) {
+        //     struct node *current = malloc(sizeof(struct node));
+        //     current = head;
+        //     printf("%s Hello\n", head->key);
+        //     while (current != NULL) {
+        //         printf("Hello\n");
+        //         if (strstr(myargs[0], current->key) != NULL) {
+        //             myargs[0] = current->data;
+        //             // myargs[1] = 0;
+        //             printf("MYARGS0: %s MYARGS1: %s\n", myargs[0], myargs[1]);
+        //             break;
+        //         }
+        //         current = current->next;
+        //     }
+        // }
+        newProcess(myargs);
+    }
+    
+}
+
+void interactive(int argc, char *argv[]) {
+    FILE *fp;
+    fp = stdin;
+    char buffer[512];
+
+    printPrompt();
+
+    while (fgets(buffer, 512, fp) != NULL) {
+        processCommand(buffer);
+        printPrompt();
+    }
+
 }
 
 int main(int argc, char *argv[]) {
     FILE *fp;
-    int flag = 0;
-    const char delim[9] = " \n\t\v\t\r";
+    char buffer[512];
+    //int flag = 0;
 
     if (argc < 1 || argc > 2) {
         write(STDERR_FILENO, "Usage: mysh [batch-file]\n", 25);
-        return (1);
+        return 1;
     }
-    else if (argc == 2) {
+    else if (argc == 1) {
+        interactive(argc, argv);
+    }
+    else {
         fp = fopen(argv[1], "r");
-        flag = 1;
         if (fp == NULL) {
             char str[] = "Error: Cannot open file ";
             strcat(str, argv[1]);
             strcat(str, ".\n");
             write(STDERR_FILENO, str, strlen(str));
-            return (1);
-        }
-    }
-    else {
-        fp = stdin;
-        printPrompt();
-    }
-
-    char buffer[512];
-    char *myargs[sizeof(buffer)];
-    char *token;
-    //struct node *head = NULL;
-
-    while (fgets(buffer, 512, fp)) {
-        int j = 0;
-        while (myargs[j] != NULL) {
-            myargs[j] = 0;
-            j++;
+            return 1;
         }
 
-        if (flag == 1) {
+        while (fgets(buffer, 512, fp) != NULL) {
             write(1, buffer, strlen(buffer));
+            processCommand(buffer);
         }
-
-        if (checkWhiteSpace(buffer) == 0) {
-            if (flag == 0) {
-                printPrompt();
-            }
-            continue;
-        }
-
-        token = strtok(buffer, delim);
-        myargs[0] = token;
-        int count = 1;
-        while (token != NULL) {
-            token = strtok(NULL, delim);
-            myargs[count] = token;
-            count++;
-        }
-        myargs[count] = '\0';
-
-        if (strcmp(myargs[0], "alias") == 0) {
-            printf("alias mode");
-            alias(myargs, count -1);
-        }
-        else if (strcmp(myargs[0], "unalias") == 0) {
-            printf("unalilas mode");
-            unalias(myargs, count - 1);
-        }
-        else {
-            // base case -- break the line into array of arguments
-            // token = strtok(buffer, delim);
-            // myargs[0] = token;
-            // int i = 1;
-            // while (token != NULL) {
-            //     token = strtok(NULL, delim);
-            //     myargs[i] = token;
-            //     i++;
-            // }
-            // check if arg[0] belongs to the linked list
-            // if (head != NULL) {
-            //     struct node *current = malloc(sizeof(struct node));
-            //     current = head;
-            //     printf("%s Hello\n", head->key);
-            //     while (current != NULL) {
-            //         printf("Hello\n");
-            //         if (strstr(myargs[0], current->key) != NULL) {
-            //             myargs[0] = current->data;
-            //             // myargs[1] = 0;
-            //             printf("MYARGS0: %s MYARGS1: %s\n", myargs[0], myargs[1]);
-            //             break;
-            //         }
-            //         current = current->next;
-            //     }
-            // }
-        }
-        // printf("reached process");
-        newProcess(myargs);
-        if (argc == 1) {
-            printPrompt();
-            continue;
-        }
+        fclose(fp);
     }
-    return (0);
+    return 0;
 }
-
-// Batch mode - arg[1] is the name of a file
-// 1. check if file is valid, output error if not
-// 2. open file, read line to stdout
-// 3. complete command listed in file
-// 4. repeat, put into loop
-
-// Interactive mode - output a prompt for user to type commands to
-// question: which commands do we need to include?
-// ls in tests,
-
-// redirection - output goes into a file rather than stdout
-// if exists, overwrite, else create a new file
-
-// aliasing = shortcuts
